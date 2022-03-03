7893da7e2ced19d03a2b19f872d45c1a0555bf05
Jul10CC <julio1cayax@outlook.es>
2022-03-03 12:44:02 -0600

    Array de Peliculas JSON

--- peliculas/src/App.js
+++ peliculas/src/App.js
@@ -12,7 +12,8 @@ function App() {
       actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
       duracion: "2h21’",
       fecha: "1 May 2015",
-      img: "images/uploads/mv1.jpg"
+      img: "images/uploads/mv1.jpg",
+      descripcion : " Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
     },
     {
       titulo: "into the wild  (2014)",
@@ -21,22 +22,21 @@ function App() {
       actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
       duracion: "2h21’",
       fecha: "1 May 2015",
-      img: "images/uploads/mv2.jpg"
+      img: "images/uploads/mv2.jpg", 
+      descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
     },
-  ]
+  ];
+
   return (
     <PageWrapper>
-      <Pelicula titulo="oblivion (2012)" calificacion="8.1" director="Joss Whedon"
-        actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" duracion="2h21’" fecha="1 May 2015"
-        img="images/uploads/mv1.jpg">
-        Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
-      </Pelicula>
+      {peliculas.map(pelicula =>
+        <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion} director={pelicula.director}
+          actores={pelicula.actores} duracion={pelicula.duracion} fecha={pelicula.fecha}
+          img={pelicula.img} >
+        {pelicula.descripcion}
+        </Pelicula>
+      )}
 
-      <Pelicula titulo="into the wild  (2014)" calificacion="7.8" director="Joss Whedon"
-        actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" duracion="2h21’" fecha="1 May 2015"
-        img="images/uploads/mv2.jpg">
-        As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat...
-      </Pelicula>
     </PageWrapper>
   );
 }
